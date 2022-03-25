e93e6aeb5035278e247ebfc3f623b53a2f69fcc5
Bentheminernz <65065992+Bentheminernz@users.noreply.github.com>
2022-03-25 20:07:32 +1300

    Update

--- Games/AstronautRun/sketch.js
+++ Games/AstronautRun/sketch.js
@@ -1,110 +1,162 @@
 function setup() {
-  createCanvas(665, 480);
-  bgColor = 'black';
-  alienPic = createImg('alien1.jpg');
-  alienPic.hide();
-  alienX = 0;
-  alienY = 430;
-  alienWidth = 50;
-  alienHeight = 50;
-  alienSpeed = 0.03;
-  points = 0;
-  frameRate(60);
-  noLoop();
-  // create button
-  button = createButton('Start');
-  button.position(-300, -480, 'relative');
-  button.style('background', 'lime');
-  button.style('color', 'white');
-  button.mouseClicked(startGame);
-  // create text
-  gameMessage = createElement('h1');
-  gameMessage.position(400, 300);
-  gameMessage.style('font-style', 'italic');
-  gameMessage.style('color', 'gold');
-  newSession = true;
+  createCanvas(620, 420);
+  player = new Player();
+  obstacle = new Obstacle();
+  obstacles = [];
+  asteroid = new Asteroid();
+  asteroids = [];
+  randomDistance = int(random(65, 150));
+  frames = 0;
+  score = 0;
+  astroImgStand = createImg('astroStand.png');
+  astroImgStand.hide();
+  astroImgDuck = createImg('astroDuck.png');
+  astroImgDuck.hide();
+  alienImg = createImg('alien2.png');
+  alienImg.hide();
+  asteroidImg = createImg('asteroid.png');
+  asteroidImg.hide();
+  astroImg = astroImgStand;
+  lost = false;
+  highScore = 0;
 }
 
-function draw() {
-  background(bgColor);
-  // draws alien image
-  image(alienPic, alienX, alienY, alienWidth, alienHeight);
-  textSize(30);
-  fill('white');
-  text(points, 600, 25);
-  textSize(30);
-  fill('white');
-  text('Level: ' + stage, 300, 50);
-  // makes the alien follow the mouse
-  distanceX = mouseX - alienX;
-  alienX = distanceX * alienSpeed + alienX;
-  distanceY = mouseY - alienY;
-  alienY = distanceY * alienSpeed + alienY;
-  // keeps alien inside the canvas
-  alienX = constrain(alienX, 0, width - alienWidth);
-  alienY = constrain(alienY, 0, height - alienHeight);
-  // if alien touches mouse, stop game
-  if (mouseX >= alienX - 1 && mouseX <= alienX + alienWidth && mouseY >= alienY - 1 && mouseY <= alienY + alienHeight) {
-    gameMessage.html('Game Over');
-    stopGame();
-  }
-  if (mouseX > width || mouseX < 0 || mouseY > height || mouseY < 0) {
-    gameMessage.html('Out of Bounds');
-    stopGame();
-  }
-  if (newSession === true) {
-    gameMessage.html('Click start to begin');
-  }
-  if (frameCount % 60 === 0) {
-    points++;
-    if (points % 10 === 0) {
-      alienSpeed = alienSpeed + 0.03;
-      changeBgColor(points);
+function keyPressed() {
+  if (key === ' ' || keyCode === UP_ARROW) {
+    player.jump();
+    if (lost === true) {
+      restartGame();
     }
   }
 }
 
-function stopGame() {
-  background('navy');
-  textSize(30);
-  fill('white');
-  text(points, 600, 25);
-  noLoop();
-  alienX = 50;
-  alienY = 550;
+function updateHighScore() {
+  if (score > highScore) {
+    highScore = score;
+  }
 }
 
-function startGame() {
-  stage = 'Easy';
-  points = 0;
-  gameMessage.html('');
-  bgColor = 'black';
-  newSession = false;
-  alienSpeed = 0.03;
+function restartGame() {
+  score = 0;
+  lost = false;
+  obstacles = [];
+  asteroids = [];
+  frames = 0;
+  player = new Player();
+  new Obstacle();
+  randomDistance = int(random(65, 150));
   loop();
 }
 
-function changeBgColor(points) {
-  switch (points) {
-    case 10:
-      bgColor = 'green';
-      stage = 'Intermediate';
-      break;
-    case 20:
-      bgColor = 'blue';
-      stage = 'Hard';
-      break;
-    case 30:
-      bgColor = 'yellow';
-      stage = 'Advanced';
-      break;
-    case 40:
-      bgColor = 'red';
-      stage = 'Expert';
-      break;
-    case 50:
-      bgColor = 'black';
-      stage = 'Boss';
-      break;
+function draw() {
+  background('teal');
+  textSize(24);
+  fill('white');
+  text(score, 5, 24);
+  score++;
+  frames++;
+  if (frames === randomDistance) {
+    obstacles.push(new Obstacle());
+    frames = 0;
+    randomDistance = int(random(65, 150));
+  }
+  for (let obstacle of obstacles) {
+    if (player.hits(obstacle)) {
+      background('red');
+      text(score, 5, 24);
+      lost = true;
+      updateHighScore();
+      noLoop();
+    }
+    obstacle.move();
+    obstacle.display();
+  }
+  if (frames === randomDistance - 50 && randomDistance > 100) {
+    asteroids.push(new Asteroid());
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    player.duck();
+  } else {
+    player.stand();
+  }
+  for (let asteroid of asteroids) {
+    if (player.hits(asteroid)) {
+      background('red');
+      text(score, 5, 24);
+      lost = true;
+      updateHighScore();
+      noLoop();
+    }
+    asteroid.fly();
+    asteroid.display();
+  }
+  text('High score: ' + highScore, 400, 24);
+  player.display();
+  player.move();
+}
+
+class Player {
+  constructor() {
+    this.height = 150;
+    this.width = 50;
+    this.x = 0;
+    this.y = height - this.height;
+    this.color = 'lime';
+    this.velocity = 0;
+    this.gravity = 0.6;
+  }
+  display() {
+    image(astroImg, this.x, this.y, this.width, this.height);
+  }
+  jump() {
+    if (this.y == height - this.height) {
+      this.velocity = 17;
+    }
+  }
+  move() {
+    this.y = this.y - this.velocity;
+    this.velocity = this.velocity - this.gravity;
+    this.y = constrain(this.y, 0, height - this.height);
+  }
+  hits(item) {
+    return collideRectRect(this.x, this.y, this.width, this.height, item.x, item.y, item.width, item.height);
+  }
+  duck() {
+    this.height = 50;
+    astroImg = astroImgDuck;
+  }
+  stand() {
+    this.height = 150;
+    astroImg = astroImgStand;
+  }
+}
+
+class Obstacle {
+  constructor() {
+    this.width = random(20, 40);
+    this.height = random(50, 80);
+    this.x = width;
+    this.y = height - this.height;
+  }
+  display() {
+    image(alienImg, this.x, this.y, this.width, this.height);
+  }
+  move() {
+    this.x = this.x - 6;
+  }
+}
+
+class Asteroid {
+  constructor() {
+    this.width = random(120, 150);
+    this.height = random(120, 150);
+    this.x = width - 20;
+    this.y = 200;
+  }
+  display() {
+    image(asteroidImg, this.x, this.y, this.width, this.height);
+  }
+  fly() {
+    this.x = this.x - 6;
   }
 }
